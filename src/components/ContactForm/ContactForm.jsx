ef71e9ff6e42ad2f3e437a1b62f209aaeaa307ab
OlhaMasl <maslakok@gmail.com>
2024-07-15 13:38:00 +0300

    add ContactForm component

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,19 +1,32 @@
 import { Field, Form, Formik } from "formik";
 import s from "./ContactForm.module.css";
+import { nanoid } from "nanoid"
 
-const ContactForm = ({onAdd}) => {
+const ContactForm = ({ onAdd }) => {
+    const initialValues = {
+        id: nanoid(),
+        name: "",
+        number: "",
+    }
+
+    const handleSubmit = data => {
+        console.log(data);
+        return onAdd(data);
+    }
+
     return (
         <div>
-            <Formik initialValues={{}} onSubmit={() => {}}>
-                <Form>
-                    <lebel>
+            <Formik initialValues={{initialValues}} onSubmit={handleSubmit}>
+                <Form className={s.form}>
+                    <label className={s.dataFielf}>
                         <span>Name</span>
-                        <Field name="name"></Field>
-                    </lebel>
-                    <lebel>
+                        <Field type="text" name="name"></Field>
+                    </label>
+                    <label className={s.dataFielf}>
                         <span>Number</span>
-                        <Field name="name"></Field>
-                    </lebel>
+                        <Field  type="text" name="number"></Field>
+                    </label>
+                    <button type="submit" className={ s.addContactBtn}>Add contact</button>
                 </Form>
             </Formik>
         </div>
